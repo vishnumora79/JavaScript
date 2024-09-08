5d0012e7978c7f89a38604524c7abd85eec20e57
vishnu mora <moravishnu2005@gmail.com>
2024-09-08 17:47:22 +0530

    Covered all the basics of JS

--- Basics/js_3.js
+++ Basics/js_3.js
@@ -88,7 +88,7 @@
 // Arrays
 // Collection of elements - [] or new Array()
 
-var countries = ["India", "USA", "Japan", "Russia"];
+// var countries = ["India", "USA", "Japan", "Russia"];
 
 // push - Appends new elements to the end of an array, and returns the new length of the array.
 // pop - Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
@@ -98,7 +98,7 @@ var countries = ["India", "USA", "Japan", "Russia"];
 // foreach - Performs the specified action for each element in an array.
 
 // Try to add remaining methods of array when you got time.
-var states = new Array("AP", "Tamil Nadu", "Ts", "Karnataka", "Kerala");
+// var states = new Array("AP", "Tamil Nadu", "Ts", "Karnataka", "Kerala");
 
 // console.log(countries);
 // console.log(countries[0]); // Aceessing elements
@@ -113,9 +113,85 @@ var states = new Array("AP", "Tamil Nadu", "Ts", "Karnataka", "Kerala");
 // states.shift();
 // console.log(states);
 
-console.log(states.push(4));
-var newLength = states.push(5);
-console.log(newLength);
+// console.log(states.push(4));
+// var newLength = states.push(5);
+// console.log(newLength);
+
+
+// Arrow functions
+
+// Normal arrow function
+// var arrowFun = () => {
+//     return "This is an arrow function";
+// }
+
+// console.log(arrowFun());
+
+
+// Arrow function with parameters
+// var arrowFun = (element) => {
+//     var x = 6;
+//     var y = 7;
+//     var z = 8;
+//     return element + x + y + z + 7;
+// }
+
+// console.log(arrowFun(2));
+
+// // Another way to write an arrow function
+// var arrowFun = (a, b) => a + b;
+// console.log(arrowFun(2, 5));
+
+
+// Call back function example
+// Definition : Any function that is passed as an argument to another function so that it can be executed
+// in that other function is called as a callback function.
+
+// var ans = [2, 4 ,6 ,8].every((e) => {
+//     return e % 2 === 0;
+// });
+
+// console.log(ans);
+
+// every() : applies condition which is there in call back function to all the elements in array
+// return true if all elements in array satisfy the condition otherwise false
+
+// fill() : fill the array with given elements
+// 1.
+// var arr = [2,3,4,5,6,7];
+// console.log(arr);
+// arr.fill("v");
+// console.log(arr);
+
+// 2.
+// var arr = [2,3,4,5,6,7];
+// arr.fill("v", 2, 5);
+// console.log(arr);
+
+
+
+// filter() : expects a call back function
+// Filter elements based on call back
+// return new array
+
+// const myNumber = [2,3,5,6,3,2,5];
+
+// const ans = myNumber.filter((num) => num > 3);
+// console.log(ans);
+
+
+// slice() : splices the array into two parts
+
+// let arr = [2,3,5,6,3];
+// let ans = arr.slice(2,4);
+// console.log(ans);
+
+
+// splice() : parameters are start index, count, content to replace the count of values from start index
+
+var users = ["Hitesh", "Vishnu", "Striver", "Kunal"];
+users.splice(1, 1, "Good to go");
+console.log(users)
 
 
 
